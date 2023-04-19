2f70d2a9e53cb4c59874a919f591061869189c96
jjunlob074 <jjunlob074@g.educaand.es>
2023-04-19 12:02:57 +0200

    LIMPIEZA

---
+++ web/sites/default/files/php/twig/643fb135bc7e8_node--peliculas--pill.htm_9P81-9RjdKAZasrK4TWIqGs7W/tX8Jh2PIZ0UZNzhfKT3dvxUp5spZmyNsxI93SwAIXUw.php
@@ -0,0 +1,231 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/stable9_child/templates/content/node--peliculas--pill.html.twig */
+class __TwigTemplate_72d4b0e8187eab45b304631170633d50 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 70
+        echo "
+<article";
+        // line 71
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 71, $this->source), "html", null, true);
+        echo ">
+    <div class=\"node-peliculas-pill\">
+    <div class=\"row\">
+      <div class=\"c-image-card col-xs-12 col-md-12 col-lg-12 col-xl-3 \">
+        <div class=\"c-image\">
+          ";
+        // line 76
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 76, $this->source), "html", null, true);
+        echo "
+
+        </div>
+      </div>
+        <div class=\"c-link col-xs-12 col-md-12 col-lg-12 col-xl-9 \">
+          <div class=\"c-link\">
+  ";
+        // line 82
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 82, $this->source), "html", null, true);
+        echo "
+  ";
+        // line 83
+        if ((($context["label"] ?? null) &&  !($context["page"] ?? null))) {
+            // line 84
+            echo "    <h2";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_attributes"] ?? null), 84, $this->source), "html", null, true);
+            echo ">
+      <a href=\"";
+            // line 85
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 85, $this->source), "html", null, true);
+            echo "\" rel=\"bookmark\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 85, $this->source), "html", null, true);
+            echo "</a>
+    </h2>
+  ";
+        }
+        // line 88
+        echo "  ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 88, $this->source), "html", null, true);
+        echo "
+           </div>
+          </div>
+        </div>
+      </div>
+
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/stable9_child/templates/content/node--peliculas--pill.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  78 => 88,  70 => 85,  65 => 84,  63 => 83,  59 => 82,  50 => 76,  42 => 71,  39 => 70,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a node.
+ *
+ * Available variables:
+ * - node: The node entity with limited access to object properties and methods.
+ *   Only method names starting with \"get\", \"has\", or \"is\" and a few common
+ *   methods such as \"id\", \"label\", and \"bundle\" are available. For example:
+ *   - node.getCreatedTime() will return the node creation timestamp.
+ *   - node.hasField('field_example') returns TRUE if the node bundle includes
+ *     field_example. (This does not indicate the presence of a value in this
+ *     field.)
+ *   - node.isPublished() will return whether the node is published or not.
+ *   Calling other methods, such as node.delete(), will result in an exception.
+ *   See \\Drupal\\node\\Entity\\Node for a full list of public properties and
+ *   methods for the node object.
+ * - label: (optional) The title of the node.
+ * - content: All node items. Use {{ content }} to print them all,
+ *   or print a subset such as {{ content.field_example }}. Use
+ *   {{ content|without('field_example') }} to temporarily suppress the printing
+ *   of a given child element.
+ * - author_picture: The node author user entity, rendered using the \"compact\"
+ *   view mode.
+ * - metadata: Metadata for this node.
+ * - date: (optional) Themed creation date field.
+ * - author_name: (optional) Themed author name field.
+ * - url: Direct URL of the current node.
+ * - display_submitted: Whether submission information should be displayed.
+ * - attributes: HTML attributes for the containing element.
+ *   The attributes.class element may contain one or more of the following
+ *   classes:
+ *   - node: The current template type (also known as a \"theming hook\").
+ *   - node--type-[type]: The current node type. For example, if the node is an
+ *     \"Article\" it would result in \"node--type-article\". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node--view-mode-[view_mode]: The View Mode of the node; for example, a
+ *     teaser would result in: \"node--view-mode-teaser\", and
+ *     full: \"node--view-mode-full\".
+ *   The following are controlled through the node publishing options.
+ *   - node--promoted: Appears on nodes promoted to the front page.
+ *   - node--sticky: Appears on nodes ordered above other non-sticky nodes in
+ *     teaser listings.
+ *   - node--unpublished: Appears on unpublished nodes visible only to site
+ *     admins.
+ * - title_attributes: Same as attributes, except applied to the main title
+ *   tag that appears in the template.
+ * - content_attributes: Same as attributes, except applied to the main
+ *   content tag that appears in the template.
+ * - author_attributes: Same as attributes, except applied to the author of
+ *   the node tag that appears in the template.
+ * - title_prefix: Additional output populated by modules, intended to be
+ *   displayed in front of the main title tag that appears in the template.
+ * - title_suffix: Additional output populated by modules, intended to be
+ *   displayed after the main title tag that appears in the template.
+ * - view_mode: View mode; for example, \"teaser\" or \"full\".
+ * - teaser: Flag for the teaser state. Will be true if view_mode is 'teaser'.
+ * - page: Flag for the full page state. Will be true if view_mode is 'full'.
+ * - readmore: Flag for more state. Will be true if the teaser content of the
+ *   node cannot hold the main body content.
+ * - logged_in: Flag for authenticated user status. Will be true when the
+ *   current user is a logged-in member.
+ * - is_admin: Flag for admin user status. Will be true when the current user
+ *   is an administrator.
+ *
+ * @see template_preprocess_node()
+ *
+ */
+#}
+
+<article{{ attributes }}>
+    <div class=\"node-peliculas-pill\">
+    <div class=\"row\">
+      <div class=\"c-image-card col-xs-12 col-md-12 col-lg-12 col-xl-3 \">
+        <div class=\"c-image\">
+          {{ content }}
+
+        </div>
+      </div>
+        <div class=\"c-link col-xs-12 col-md-12 col-lg-12 col-xl-9 \">
+          <div class=\"c-link\">
+  {{ title_prefix }}
+  {% if label and not page %}
+    <h2{{ title_attributes }}>
+      <a href=\"{{ url }}\" rel=\"bookmark\">{{ label }}</a>
+    </h2>
+  {% endif %}
+  {{ title_suffix }}
+           </div>
+          </div>
+        </div>
+      </div>
+
+</article>
+", "themes/custom/stable9_child/templates/content/node--peliculas--pill.html.twig", "/var/www/html/web/themes/custom/stable9_child/templates/content/node--peliculas--pill.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 83);
+        static $filters = array("escape" => 71);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
